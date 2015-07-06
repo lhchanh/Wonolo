a30ed3c50fa882c66de2ffdb7c717eb148f7c1d7
Chanh Le <lehoangchanh.uit@gmail.com>
2015-07-07 03:20:20 +0700

    Add Leverage browser caching
    Fix time sleep

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Leverage browser caching
+  config.static_cache_control = "public, max-age=31536000"
 end
