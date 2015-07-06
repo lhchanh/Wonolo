3b4da75b2ff6f75a0edeb57483f7ed690d278a23
Chanh Le <lehoangchanh.uit@gmail.com>
2015-07-07 01:51:03 +0700

    improve time sleep

--- app/services/instagram_service.rb
+++ app/services/instagram_service.rb
@@ -10,12 +10,12 @@ class InstagramService < BaseService
             distance = ''
             while !distance.is_a? Numeric
               begin
-                sleep 1
+                sleep 0.25
                 distance = Geocoder::Calculations.bearing_between("#{e.location.latitude}, #{e.location.longitude}", "#{lat}, #{lng}")
               rescue
                 # Fixed issue Usage Limits for Google Geocoding API
                 # 5 requests per second.
-                sleep 2
+                sleep 0.5
                 distance = Geocoder::Calculations.bearing_between("#{e.location.latitude}, #{e.location.longitude}", "#{lat}, #{lng}").to_f
               end
             end
